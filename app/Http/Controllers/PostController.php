2e6a1f913e997419089c0e96cdec5e26058aa77d
OY16-05 <yuiokuda0616@gmail.com>
2025-02-23 07:37:51 +0000

    Blog setting

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,4 +12,14 @@ class PostController extends Controller
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
         //getPaginateByLimit()はPost.phpで定義したメソッドです。
     }
+    /**
+     * 特定のIDのpostを表示する
+     * 
+     * @params Object Post //引数の$postはid=1のPostインスタンス
+     * @return Reposnse post view
+     */
+    public function show(Post $post)
+    {
+         return view('posts.show')->with(['post'=> $post]);
+    }
 }
