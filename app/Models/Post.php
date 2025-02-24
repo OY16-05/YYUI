4e622196f8dd00b43ae362d369cad4bdc757f525
OY16-05 <yuiokuda0616@gmail.com>
2025-02-24 14:53:08 +0000

    Blog 7-6

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illumunate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
-{
-    use HasFactory;
 
+{
+
+   use SoftDeletes; 
+   use HasFactory;
     
    protected $fillable = [
       'title',
